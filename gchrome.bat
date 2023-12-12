a68bde21d45bc067fcf300f11ff016feb1b40245
karlronsaria <andrew.mikedaniels@gmail.com>
2023-12-12 00:19:21 -0800

    feat: new images, gchrome faster

--- gchrome.bat
+++ gchrome.bat
@@ -6,7 +6,7 @@ if "%~1" EQU "-p" goto getNewCommand
 goto runCommand
 
 :getNewCommand
-set "cmd=powershell -Command """
+set "cmd=powershell -NoProfile -Command """
 set "cmd=%cmd%; %modulePath%\Get-Scripts.ps1 ^| foreach { . $_ } "
 set "cmd=%cmd%; Run-ShortcutGoogleChromeProfile %2 %3 %4 %5 %6"
 set "cmd=%cmd%"""
