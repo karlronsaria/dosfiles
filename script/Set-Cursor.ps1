f3ee3b71aef40f9c895f729daa9a59202669f09f
karlr <andrew.mikedaniels@gmail.com>
2024-09-24 02:28:26 -0700

    feat: toddmode.bat

---
+++ script/Set-Cursor.ps1
@@ -0,0 +1,61 @@
+
+function Set-Cursor {
+    function Start-CursorRefresh {
+        <#
+        .LINK
+        - url
+          - <https://superuser.com/questions/1769195/how-to-change-mouse-cursor-using-powershell-script-on-windows-11-without-restart>
+          - <https://superuser.com/users/8672/harrymc>
+          - <https://devblogs.microsoft.com/scripting/use-powershell-to-change-the-mouse-pointer-scheme/>
+        - retrieved: 2024_09_24
+        #>
+        $cSharpSig =
+@'
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+public static extern bool SystemParametersInfo(
+    uint uiAction,
+    uint uiParam,
+    uint pvParam,
+    uint fWinIni);
+'@
+
+        $cursorRefresh = Add-Type `
+            -MemberDefinition $cSharpSig `
+            -Name WinAPICall `
+            -Namespace SystemParamInfo `
+            –PassThru
+
+        $cursorRefresh::SystemParametersInfo(0x0057, 0, $null, 0)
+    }
+
+    # Path to the registry key
+    $regPath = "HKCU:\Control Panel\Cursors"
+    $systemCursorPath = "C:\Windows\Cursors"
+
+    # Specify the cursor files (replace these with your desired cursor files)
+    $cursorScheme = @{
+        "Arrow"        = "$systemCursorPath\aero_arrow.cur"
+        "Help"         = "$systemCursorPath\aero_helpsel.cur"
+        "AppStarting"  = "$systemCursorPath\aero_working.ani"
+        "Wait"         = "$systemCursorPath\aero_busy.ani"
+        "Crosshair"    = "$systemCursorPath\cross_i.cur"
+        "IBEAM"        = "$systemCursorPath\beam_i.cur"
+        "NWPen"        = "$systemCursorPath\pen_i.cur"
+        "No"           = "$systemCursorPath\no.cur"
+        "SizeNS"       = "$systemCursorPath\size_ns.cur"
+        "SizeWE"       = "$systemCursorPath\size_we.cur"
+        "SizeNWSE"     = "$systemCursorPath\size_nwse.cur"
+        "SizeNESW"     = "$systemCursorPath\size_nesw.cur"
+        "SizeAll"      = "$systemCursorPath\size_all.cur"
+        "UpArrow"      = "$systemCursorPath\up_i.cur"
+        "Hand"         = "$systemCursorPath\aero_link.cur"
+    }
+
+    # Apply the cursor scheme to the registry
+    foreach ($cursor in $cursorScheme.GetEnumerator()) {
+        Set-ItemProperty -Path $regPath -Name $cursor.Key -Value $cursor.Value
+    }
+
+    # Notify the system of the change
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+
---
+++ toddmode.bat
@@ -0,0 +1,57 @@
+@echo off
+
+if "%~1" EQU "--help" goto :help
+if "%~1" EQU "-h" goto :help
+
+set "cmd=pwsh -Command ""
+set "cmd=%cmd%Import-DemandModule theme, mouse, pointer -Mode And"
+set "cmd=%cmd%; Set-MousePointerTheme"
+
+if "%~1" EQU "" goto :setToddMode
+if "%~1" EQU "--on" goto :setToddMode
+if "%~1" EQU "--off" goto :setDefaultMode
+goto :notImplemented
+
+:setToddMode
+set "cmd=%cmd% -Name ToddMode"
+goto :completeCmd
+
+:setDefaultMode
+set "cmd=%cmd% -Name SystemDefault"
+goto :completeCmd
+
+:completeCmd
+set "cmd=%cmd% -Verbose ^| Out-Null""
+
+if "%~2" EQU "--whatif" goto :echo
+goto :execute
+
+:echo
+echo %cmd%
+exit /b
+
+:execute
+%cmd%
+exit /b
+
+:notImplemented
+echo.This theme has not been implemeneted
+exit /b
+
+:help
+echo.
+echo.Usage: %~n0 [--on^|--off [--whatif]]
+echo.
+echo.Options:
+echo.  --on       Activates Todd Mode
+echo.  --off      Deactivates Todd Mode, sets to default theme
+echo.  --whatif   Echoes the full command
+echo.
+echo.Examples:
+echo.  %~n0
+echo.  %~n0 --on
+echo.  %~n0 --off
+echo.  %~n0 --on --whatif
+echo.  %~n0 --off --whatif
+exit /b
+
